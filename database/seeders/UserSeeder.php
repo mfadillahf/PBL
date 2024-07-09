c802780a369a799113f30baf0adaac1ae81b41e1
Muhammad Fadillah Fahrurrazi <mfahrurazifadillah@gmail.com>
2024-07-09 16:01:55 +0800

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('admin'),
+            'email_verified_at' => now()
+        ]);
+    }
+}
